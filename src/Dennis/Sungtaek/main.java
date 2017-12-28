472fada950dd72823ad400d9d71566605f06365c
Dennis <Dennis@DESKTOP-V63LC0R>
2017-12-29 01:47:55 +0900

    Explain about Source

--- src/Dennis/Sungtaek/main.java
+++ src/Dennis/Sungtaek/main.java
@@ -12,18 +12,18 @@ public class main {
 		System.out.println("문자를 입력하세요.");
 		input = sc.next().charAt(0);
 		
-		if(Character.isUpperCase(input)) {
+		if(Character.isUpperCase(input)) {			//input이 Upper인지 확인
 			output = Character.toLowerCase(input);
 			System.out.println(output);
 		}
 		
-		else if(Character.isLowerCase(input)) {
+		else if(Character.isLowerCase(input)) {		//input이 Lower인지 확인
 			output = Character.toUpperCase(input);
 			System.out.println(output);
 		}
 			
 		else
-			System.out.println("잘못 입력하셨습니다.");
+			System.out.println("잘못 입력하셨습니다.");  //Upper | Lower가 아니면 처리.
 		
 	}
 
